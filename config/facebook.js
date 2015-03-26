91631c6d107c20ab8192335fa85a122eebd64c38
shameer <sameerean@gmail.com>
2015-03-26 16:08:51 +0400

    Initial commit

--- config/facebook.js
+++ config/facebook.js
@@ -4,11 +4,8 @@
  */
 module.exports.facebook = {
 
-//	FACEBOOK_CLIENT_ID : "YOUR-FACEBOOK_CLIENT_ID",
-//	FACEBOOK_CLIENT_SECRET : "YOUR-FACEBOOK_CLIENT_SECRET",
-//	FACEBOOK_AUTH_CALLBACK_URL : "http://YOUR-IP:PORT/auth/facebook/callback" /*http://localhost:1337/auth/facebook/callback*/
-		FACEBOOK_CLIENT_ID : "179057812298343",
-		FACEBOOK_CLIENT_SECRET : "a4572f97e4595bcd66998015005be02a",
-		FACEBOOK_AUTH_CALLBACK_URL : "http://localhost:1337/auth/facebook/callback"
+	FACEBOOK_CLIENT_ID : "YOUR-FACEBOOK_CLIENT_ID",
+	FACEBOOK_CLIENT_SECRET : "YOUR-FACEBOOK_CLIENT_SECRET",
+	FACEBOOK_AUTH_CALLBACK_URL : "http://YOUR-IP:PORT/auth/facebook/callback" /*http://localhost:1337/auth/facebook/callback*/
 		
 };
